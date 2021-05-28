8f1c826b3db1d2bcafa3023f5d50c3b9c54ad3d6
Shweta Meshram <shweta.meshram@ibm.com>
2021-05-28 16:28:42 +0530

    Created abc and xyz java files

--- abc.java
+++ abc.java
@@ -1,7 +1,5 @@
-public class abc
-{
-	public static void main(String args[])
-	{
-		System.out.println("abc");
-	}
+public class abc {
+   public static void main(String []args) {
+      System.out.println("abc");
+   }
 }
--- xyz.java
+++ xyz.java
@@ -1,7 +1,5 @@
-public class xbc
-{
-	public static void main(String args[])
-	{
-		System.out.println("xyz");
-	}
+public class xyz {
+   public static void main(String []args) {
+      System.out.println("xyz");
+   }
 }
